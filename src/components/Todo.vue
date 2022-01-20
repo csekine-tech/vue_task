ec46fa5d5a18b2fb5f29dc6ab10dead8bcf8dd85
csekine-tech <csekine.mscll24@gmail.com>
2022-01-20 13:50:22 +0900

    cssの調整

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,20 +1,31 @@
 <template>
-  <div id="app">
-    <h1>TODO LIST</h1>
-    <form action="" v-on:submit.prevent>
-      <input type="text" v-model="newItem" />
-      <input type="date" v-model="newDate" />
-      <button v-on:click="addItem">送信</button>
-    </form>
-    <ul>
-      <li v-for="(todo, key) in todos" :key="key">
-        <div class="line">
-          <input type="checkbox" v-model="todo.isDone" />
-          <span>{{ todo.item }}</span>
-          <span>期限:{{ todo.date }}</span>
+  <div id="app" class="container_wrapper">
+    <div class="container">
+      <h1>TODO LIST</h1>
+
+      <form action="" v-on:submit.prevent>
+        <div>
+          <input type="text" v-model="newItem" />
         </div>
-      </li>
-    </ul>
+        <div>
+          <input type="date" v-model="newDate" />
+        </div>
+        <div class="button_wrapper">
+          <button v-on:click="addItem">送信</button>
+        </div>
+      </form>
+      <ul>
+        <li v-for="(todo, key) in todos" :key="key">
+          <div class="line">
+            <input type="checkbox" v-model="todo.isDone" />
+            <span>
+              <span>{{ todo.item }}</span>
+              <span class="todo_date">期限:{{ todo.date }}</span>
+            </span>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -41,4 +52,44 @@ export default {
 </script>
 
 <style>
+.container{
+    width: 350px;
+}
+.container_wrapper{
+    display: flex;
+    justify-content: center;
+}
+ul {
+  list-style: none;
+}
+li {
+  margin-left: -30px;
+}
+input[type="text"],
+input[type="date"] {
+  margin-bottom: 10px;
+  width: 100%;
+  padding: 5px;
+  font-size: 14px;
+  box-sizing: border-box;
+}
+button {
+  border: none;
+  background-color: cadetblue;
+  color: white;
+  padding: 8px 20px;
+  cursor: pointer;
+  font-size: 16px;
+}
+.button_wrapper{
+display: flex;
+justify-content: right;
+}
+
+input[type="checkbox"]:checked + span {
+  text-decoration: line-through;
+}
+.todo_date {
+  margin-left: 10px;
+}
 </style>
